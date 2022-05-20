1b6fe0039b57062b68a525ec746279398c6916e4
Rsiesener <r.siesener91@gmail.com>
2022-05-20 11:45:48 -0500

    added button animation

--- index.js
+++ index.js
@@ -8,23 +8,38 @@ document.addEventListener('keydown', (e) => {
   if ((chosenKey === 'w')) {
 		audio = new Audio('./sounds/tom-1.mp3')
 		audio.play()
+		buttonAnimation(chosenKey)
 	} else if (chosenKey === 'a') {
 		audio = new Audio('./sounds/tom-2.mp3')
 		audio.play()
+		buttonAnimation(chosenKey)
 	} else if (chosenKey === 's') {
 		audio = new Audio('./sounds/tom-3.mp3')
 		audio.play()
+		buttonAnimation(chosenKey)
 	} else if (chosenKey === 'd') {
 		audio = new Audio('./sounds/tom-4.mp3')
 		audio.play()
+		buttonAnimation(chosenKey)
 	} else if (chosenKey === 'j') {
 		audio = new Audio('./sounds/snare.mp3')
 		audio.play()
+		buttonAnimation(chosenKey)
 	} else if (chosenKey === 'k') {
 		audio = new Audio('./sounds/crash.mp3')
 		audio.play()
+		buttonAnimation(chosenKey)
 	} else if (chosenKey === 'l') {
 		audio = new Audio('./sounds/kick-bass.mp3')
 		audio.play()
+		buttonAnimation(chosenKey)
 	} 
 })
+
+function buttonAnimation(keyPress) {
+	let button = document.querySelector('.' + keyPress)
+	button.classList.add('pressed')
+	setTimeout(() => {
+		button.classList.remove('pressed')
+	}, 200)
+}
